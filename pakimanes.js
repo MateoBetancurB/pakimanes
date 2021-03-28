5aec076664fedbc40e4bdb2cc3e7f7789b4427b3
Mateo B <mateobetancurb@gmail.com>
2021-03-28 13:19:45 -0500

    cambios 1.5

--- pakimanes.js
+++ pakimanes.js
@@ -1,8 +1,36 @@
+var imagenes = [];
+imagenes["Cauchin"] = "vaca.png";
+imagenes["Pokacho"] = "pollo.png";
+imagenes["Tocinauro"] = "cerdo.png";
+
 class Pakiman {
-    constructor(nombre) {
-        this.nombre = nombre;
-        this.tipo = "tierra";
+    constructor(n, v, a) {
+        this.imagen = new Image();
+        this.nombre = n;
+        this.vida = v;
+        this.ataque = a;
+
+        this.imagen.src = imagenes[this.nombre];
+    }
+    hablar() {
+        alert(this.nombre);
+    }
+    mostrar() {
+        document.body.appendChild(this.imagen);
+        document.write("<p>");
+        document.write("<strong>" + this.nombre + "</strong><br/>");
+        document.write("Vida: " + this.vida + "<br/>");
+        document.write("Ataque: " + this.ataque);
+        document.write("</p>");
     }
 }
 
-var cauchin = new Pakiman();
+
+var cauchin = new Pakiman("Cauchin", 100, 30);
+var pokacho = new Pakiman("Pokacho", 80, 50);
+var tocinauro = new Pakiman("Tocinauro", 120, 50);
+
+
+pokacho.mostrar();
+cauchin.mostrar();
+tocinauro.mostrar();
